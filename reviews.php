702318e52fafd7e17179019e92e7bf0bda3d5b98
Bret Wagner <bretwagner@bwagner-webdev.com>
2017-01-25 16:29:24 -0600

    Used a baseline Gravity Forms Add-on Framework
    Started off with the same framework I used to start the vinaudit plugin.
    I want to create this in the same time frame if possible. which was
    approximately 2 weeks to functionality ready for integrating into other
    services.
    
    Since this will not really be integrating into other services I think a
    3 week turn around time on this is not out of the question. Styling
    things and making them pretty may be another thing all together though.

---
+++ reviews.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Gravity Forms Review Add-On
+Plugin URI: http://www.gravityforms.com
+Description: A Gravity Forms Add-On to allow Admins to collect reviews and easily share them on their site
+Version: 0.1
+Author: Bret Wagner
+Author URI:
+
+------------------------------------------------------------------------
+*/
+/**
+ * @author Bret Wagner <bretwagner@bwagner-webdev.com>
+ * @copyright 2017 Free to copy and utilize as you see fit, just give credit if you like the plugin.
+ *
+ * Created to give site admins an easy way to create forms to collect reviews of their restaurant, hotel, or business.
+ * I wanted to create something that allowed me to easily take the reviews and post them on the site without a hassle.
+ *
+ */
+
+define( 'GF_REVIEWS_VERSION', '0.1' );
+
+add_action( 'gform_loaded', array( 'GF_Reviews_Bootstrap', 'load' ), 5 );
+
+class GF_Reviews_Bootstrap {
+
+    public static function load() {
+
+        if ( ! method_exists( 'GFForms', 'include_addon_framework' ) ) {
+            return;
+        }
+
+        require_once( 'class-gfreviews.php' );
+
+        GFAddOn::register( 'GFReviews' );
+    }
+
+}
+
+function gf_simple_addon() {
+    return GFSimpleAddOn::get_instance();
+}
---
+++ shortcodes.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @author Bret Wagner <bretwagner@bwagner-webdev.com>
+ * @copyright 2017 Free to copy and utilize just give credit if you like the plugin.
+ *
+ */
