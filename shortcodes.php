55a70c29806029c4cea7c6278580da5ba09a841b
Bret Wagner <bretwagner@bwagner-webdev.com>
2017-01-25 16:32:24 -0600

    Getting thoughts into the todo list
    Trying to get all my thoughts down into the todo list so I don't forget
    anything and underestimate time/work remaining.

--- shortcodes.php
+++ shortcodes.php
@@ -3,4 +3,7 @@
  * @author Bret Wagner <bretwagner@bwagner-webdev.com>
  * @copyright 2017 Free to copy and utilize just give credit if you like the plugin.
  *
+ * This file is specifically for the creation of the shortcodes. I will add a settings
+ * page later which will allow for users to change the defaults from within the settings
+ * for this plugin, but it may be a bit and I need to have defaults configured here first.
  */
